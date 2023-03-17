12bbbc030a16dc1fe2b85245d6c650a82eca9d7e
fergusononyeka <fergusononyekachi@gmail.com>
2023-03-17 02:42:50 +0000

    Added changes to printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Returns: Always returns 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	returns (0);
+	return (0);
 }
