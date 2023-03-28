bb56e878b53db627226e2e8f1f343f0e2f711a48
fergusononyeka <fergusononyekachi@gmail.com>
2023-03-28 20:45:27 +0000

    Task 0 done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
 #include "main.h"
 /**
  * reset_to_98 - a function that changes value
- * Return: always 0 (Success)
+ * @n: input parameter
+ * Return: void
 */
 void reset_to_98(int *n)
 {
-	int i;
-
-	i = 402;
-	n = &i;
 	*n = 98;
-	_putchar(i);
 }
 
