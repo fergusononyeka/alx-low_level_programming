20ea6be1fbe40408ee698c48c5c732c4884e887a
fergusononyeka <fergusononyekachi@gmail.com>
2023-03-29 00:48:12 +0000

    added changes to 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,8 +12,10 @@ void rev_string(char *s)
 	int count = 0;
 	int i;
 
-	while (s[count] != '0\')
-	count++;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
 	for (i = 0; i < count; i++)
 	{
 		count--;
